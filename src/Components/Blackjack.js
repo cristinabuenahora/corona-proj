1bbf5be551b58b99ba1ace91da356b3fe7db0e93
Sierra Yit <sierrayit@gmail.com>
2020-06-25 20:25:14 -0400

    Win lose logic and display

--- src/Components/Blackjack.js
+++ src/Components/Blackjack.js
@@ -4,7 +4,7 @@ import { deal, hit, stand, reset, dealerPlay } from '../Actions/actions';
 import { getPlayerHand, getDealerHand, getDealerTotal, getPlayerTotal, getCanHit, getCanStand } from '../Selectors/selectors';
 import { connect } from 'react-redux';
 
-const Blackjack = ({ deal, hit, stand, dealerPlay, reset, canHit, canStand, dealerHand, dealerTotal, playerHand, playerTotal, gameOver }) => {
+const Blackjack = ({ deal, hit, stand, dealerPlay, reset, canHit, canStand, dealerHand, dealerTotal, playerHand, playerTotal, lose, win }) => {
   const standButton = <button onClick={ stand }>Stand</button>;
   const hitButton = <button onClick={ hit }>Hit</button>;
   const resetButton = <button onClick={ reset }>Start Over</button>;
@@ -13,38 +13,48 @@ const Blackjack = ({ deal, hit, stand, dealerPlay, reset, canHit, canStand, deal
 
   return (
    <div>
-     {gameOver 
-     ? <div>
-         <p>{playerTotal === 21 ? 'BLACKJACK!! :D' : 'Game Over :('}</p>
-         {resetButton}
-       </div>
-     : dealButton}
-     <p>Dealer</p>
-     <Hand cards={ dealerHand } />
-     <p>Your Hand: { playerTotal }</p>
-     <Hand cards={ playerHand } />
-     {(!gameOver && playerTotal > 0) && <div>
-       {canStand && standButton}
-       {canHit && hitButton}
-       {!canStand && dealerPlayButton}
-     </div>}
-   </div>
+   {win
+    ? <div>
+        <p>You win!! :D</p>
+        <p>Your total:{playerTotal}</p>
+        <p>Dealer total:{dealerTotal}</p>
+        {resetButton}
+      </div>
+    : lose
+      ? <div> <p> 'You lose :('</p> {resetButton}</div>
+      :
+      <div>
+      {dealButton}
+      <p>Dealer</p>
+      <Hand cards={ dealerHand } />
+      <p>Your Hand: { playerTotal }</p>
+      <Hand cards={ playerHand } />
+      <div>
+        {canStand && standButton}
+        {canHit && hitButton}
+        {!canStand && dealerPlayButton}
+      </div>
+      </div>
+    }
+    </div>
  );
 }
 
 const mapStateToProps = (state) => {
-  const playerTotal = getPlayerTotal(state); 
+  const playerTotal = getPlayerTotal(state);
+  const dealerTotal = getDealerTotal(state);
 
   return ({
     playerHand: getPlayerHand(state),
     playerTotal,
     dealerHand: getDealerHand(state),
-    dealerTotal: getDealerTotal(state),
+    dealerTotal,
     canHit: getCanHit(state),
     canStand: getCanStand(state),
-    gameOver: playerTotal >= 21
+    gameOver: playerTotal >= 21,
+    win: playerTotal === 21 || dealerTotal > 21
   });
-}; 
+};
 
 export default connect(mapStateToProps, {
   deal,
